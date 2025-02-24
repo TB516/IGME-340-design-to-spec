e64c455358e536e81df0355210f79bcd30f9515a
Thomas <77514919+TB516@users.noreply.github.com>
2025-02-23 20:41:10 -0500

    Added color constants

---
+++ lib/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  Colors._();
+
+  static const Color mainBackground = Color(0xFF35353D);
+  static const Color appBar = Color(0xFFA19FA2);
+  static const Color text = Color(0xFFFFFFFF);
+  static const Color gradientDark = Color(0xFF374947);
+  static const Color gradientLight = Color(0xFF828C8E);
+  static const Color itemBackground = Color(0xFF324D44);
+  static const Color textAreas = Color(0xFF686868);
+}
